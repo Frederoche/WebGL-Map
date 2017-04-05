87fb2aa9d5d0a86aa743d70b72bf87eff82fbcb6
Frederic Dumont-Kristiansen <frederic.dumont.kristiansen@norkart.no>
2017-04-05 14:05:07 +0200

    remove search bar

--- 3D1/Client/Scripts/Quadtree.js
+++ 3D1/Client/Scripts/Quadtree.js
@@ -1,24 +1,23 @@
-﻿XMap.Quadtree = function(option)
-{
+﻿XMap.Quadtree = function(option) {
     this.initialRootSize = option.initialRootSize;
-    
+
     this.chunck = new XMap.Chunck(option.chunckSize);
     this.chunck.create();
     this.nodeCenter = vec3.create();
 
     this.initialtexturePath = option.initialtexturePath;
     this.initialElevationPath = option.initialElevationPath;
-    
+
     var rootNodeOption = {
-        translation:vec3.create([0, 0, 0]),
+        translation: vec3.create([0, 0, 0]),
         scaling: option.initialRootSize,
-        color:vec3.create([255, 0, 0]),
-        center : vec3.create([0, 0, 0]),
+        color: vec3.create([255, 0, 0]),
+        center: vec3.create([0, 0, 0]),
         texturePath: "",
-        elevationDataTexturePath:"",
-        parent:undefined, 
-        bbox:this.chunck.bbox,
-        id:''
+        elevationDataTexturePath: "",
+        parent: undefined,
+        bbox: this.chunck.bbox,
+        id: ''
     };
 
     this.rootNode = new XMap.QuadtreeNode(rootNodeOption);
@@ -28,22 +27,20 @@
 };
 
 
-XMap.Quadtree.prototype =
-{
+XMap.Quadtree.prototype = {
 
-    _addNode: function (node)
-    {
+    _addNode: function(node) {
         var scaling = node.scaleFactor * 0.5;
 
         var centerUpperLeft = vec3.create();
         vec3.add(node.center, vec3.create([-scaling, 0, scaling]), centerUpperLeft);
-           
+
         var centerUpperRight = vec3.create();
         vec3.add(node.center, vec3.create([scaling, 0, scaling]), centerUpperRight);
-           
+
         var centerLowerLeft = vec3.create();
         vec3.add(node.center, vec3.create([-scaling, 0, -scaling]), centerLowerLeft);
-           
+
         var centerLowerRight = vec3.create();
         vec3.add(node.center, vec3.create([scaling, 0, -scaling]), centerLowerRight);
 
@@ -53,143 +50,129 @@ XMap.Quadtree.prototype =
             color: vec3.create([255, 0, 0]),
             texturePath: this.initialtexturePath,
             depth: node.depth - 1,
-            elevationDataTexturePath:this.initialElevationPath,
-            parent:node, 
-            bbox:this.chunck.bbox,
-            id:node.id + "2"
-            
+            elevationDataTexturePath: this.initialElevationPath,
+            parent: node,
+            bbox: this.chunck.bbox,
+            id: node.id + "2"
+
         };
 
         var upperRightOptions = {
-            translation:centerUpperRight,
-            scaling:scaling,
-            color:vec3.create([0, 255, 0]),
+            translation: centerUpperRight,
+            scaling: scaling,
+            color: vec3.create([0, 255, 0]),
             texturePath: this.initialtexturePath,
-            depth:node.depth - 1,
-            elevationDataTexturePath:this.initialElevationPath,
-            parent:node, 
-            bbox:this.chunck.bbox,
-            id: node.id +"3"
+            depth: node.depth - 1,
+            elevationDataTexturePath: this.initialElevationPath,
+            parent: node,
+            bbox: this.chunck.bbox,
+            id: node.id + "3"
         };
 
         var lowerLeftOptions = {
-            translation:centerLowerLeft,
-            scaling:scaling,
-            color:vec3.create([0, 0, 255]),
+            translation: centerLowerLeft,
+            scaling: scaling,
+            color: vec3.create([0, 0, 255]),
             texturePath: this.initialtexturePath,
-            depth:node.depth - 1,
-            elevationDataTexturePath:this.initialElevationPath,
-            parent:node, 
-            bbox:this.chunck.bbox,
-            id:node.id + "0"
+            depth: node.depth - 1,
+            elevationDataTexturePath: this.initialElevationPath,
+            parent: node,
+            bbox: this.chunck.bbox,
+            id: node.id + "0"
         };
 
-        var lowerRightOptions = 
-        {
-            translation:centerLowerRight,
-            scaling:scaling,
-            color:vec3.create([255, 0, 255]),
+        var lowerRightOptions = {
+            translation: centerLowerRight,
+            scaling: scaling,
+            color: vec3.create([255, 0, 255]),
             texturePath: this.initialtexturePath,
-            depth:node.depth - 1,
-            elevationDataTexturePath:this.initialElevationPath,
-            parent:node, 
-            bbox:this.chunck.bbox,
-            id:node.id + "1"
+            depth: node.depth - 1,
+            elevationDataTexturePath: this.initialElevationPath,
+            parent: node,
+            bbox: this.chunck.bbox,
+            id: node.id + "1"
         };
-               
-        node.child[0]  = new XMap.QuadtreeNode(upperLeftOptions);
-        node.child[1]  = new XMap.QuadtreeNode(upperRightOptions);
-        node.child[2]  = new XMap.QuadtreeNode(lowerLeftOptions);
-        node.child[3]  = new XMap.QuadtreeNode(lowerRightOptions);
+
+        node.child[0] = new XMap.QuadtreeNode(upperLeftOptions);
+        node.child[1] = new XMap.QuadtreeNode(upperRightOptions);
+        node.child[2] = new XMap.QuadtreeNode(lowerLeftOptions);
+        node.child[3] = new XMap.QuadtreeNode(lowerRightOptions);
 
         node.type = 1;
-        
+
     },
 
-    setMatrixUniforms : function(projMatrix, viewMatrix, camera)
-    {
-        this.chunck.setMatrixUniforms(projMatrix, viewMatrix,  camera);
+    setMatrixUniforms: function(projMatrix, viewMatrix, camera) {
+        this.chunck.setMatrixUniforms(projMatrix, viewMatrix, camera);
     },
 
-    setBBoxMatrixUniforms: function (projMatrix, viewMatrix)
-    {
+    setBBoxMatrixUniforms: function(projMatrix, viewMatrix) {
         this.chunck.setBBoxMatrixUniforms(projMatrix, viewMatrix);
     },
 
-    setProgram: function ()
-    {
+    setProgram: function() {
         this.chunck.setProgram();
     },
 
-    setBBoxProgram : function() {
+    setBBoxProgram: function() {
         this.chunck.setBBoxProgram();
     },
 
-    disableProgram: function ()
-    {
+    disableProgram: function() {
         this.chunck.disableProgram();
     },
 
-    removechild:function(parent)
-    {
-        for(var i =0 ; i < parent.child.length;i++)
-        {
-            if(parent.child[i].texture!==null && parent.child[i].texture.image!==null)
-            {
-                
-                parent.child[i].texture.image.removeEventListener("load",parent.child[i].loadtextureHandler, false);
+    removechild: function(parent) {
+        for (var i = 0; i < parent.child.length; i++) {
+            if (parent.child[i].texture !== null && parent.child[i].texture.image !== null) {
+
+                parent.child[i].texture.image.removeEventListener("load", parent.child[i].loadtextureHandler, false);
                 parent.child[i].texture.image = null;
                 device.deleteTexture(parent.child[i].texture);
                 parent.child[i].texture = null;
             }
 
-            if(parent.child[i].elevation!==null && parent.child[i].elevation.image!==null)
-            {
-                
-                parent.child[i].elevation.image.removeEventListener("load",parent.child[i].loadElevation, false);
+            if (parent.child[i].elevation !== null && parent.child[i].elevation.image !== null) {
+
+                parent.child[i].elevation.image.removeEventListener("load", parent.child[i].loadElevation, false);
                 parent.child[i].elevation.image = null;
                 device.deleteTexture(parent.child[i].elevation);
                 parent.child[i].elevation = null;
             }
 
-            if(parent.child[i].child.length > 0)
-            {
+            if (parent.child[i].child.length > 0) {
                 this.removechild(parent.child[i])
                 parent.child[i].parent = null;
-                parent.child[i] = null; 
+                parent.child[i] = null;
             }
         }
     },
 
-    //node.type == 2 ---->leaf
-    draw: function (wireframe, frustum, node, ext, delta, tile) {
-        
-        if (node === undefined) 
-        {
+
+    draw: function(wireframe, frustum, node, ext, delta, tile) {
+
+        if (node === undefined) {
             return;
         }
 
-        if(!frustum.isBoxInsideFrustum(node.bbox)){
-           
-            if(node.child.length > 0)
-            {
+        if (!frustum.isBoxInsideFrustum(node.bbox)) {
+
+            if (node.child.length > 0) {
                 this.removechild(node);
             }
 
             node.child = [];
 
-            if(node.texture!==null && node.texture.image!==null)
-            {
-                
-                node.texture.image.removeEventListener("load",node.loadtextureHandler, false);
+            if (node.texture !== null && node.texture.image !== null) {
+
+                node.texture.image.removeEventListener("load", node.loadtextureHandler, false);
                 node.texture.image = null;
                 device.deleteTexture(node.texture);
             }
 
-            if(node.elevation!==null && node.elevation.image!==null)
-            {
-                
-                node.elevation.image.removeEventListener("load",node.loadElevation, false);
+            if (node.elevation !== null && node.elevation.image !== null) {
+
+                node.elevation.image.removeEventListener("load", node.loadElevation, false);
                 node.elevation.image = null;
                 device.deleteTexture(node.elevation);
             }
@@ -197,19 +180,18 @@ XMap.Quadtree.prototype =
             return;
         }
 
-        if(frustum.isBoxInsideFrustum(node.bbox) && node.child.length < 4){
+        if (frustum.isBoxInsideFrustum(node.bbox) && node.child.length < 4) {
             this._addNode(node);
         }
-        
+
         //should be the closest point not the center.
         this.chunckDistFromCamera = Math.sqrt((frustum.position[0] - node.center[0]) * (frustum.position[0] - node.center[0]) +
-                                              (frustum.position[1] - node.center[1]) * (frustum.position[1] - node.center[1]) +
-                                              (frustum.position[2] - node.center[2]) * (frustum.position[2] - node.center[2]));
-        
-        if (delta / this.chunckDistFromCamera <= 0.015  && node.type === 1) {
-            
-            if (tile !== node.initialtexturePath)
-            {
+            (frustum.position[1] - node.center[1]) * (frustum.position[1] - node.center[1]) +
+            (frustum.position[2] - node.center[2]) * (frustum.position[2] - node.center[2]));
+
+        if (delta / this.chunckDistFromCamera <= 0.015 && node.type === 1) {
+
+            if (tile !== node.initialtexturePath) {
                 node.updateTexturePath(tile);
             }
 
@@ -217,24 +199,22 @@ XMap.Quadtree.prototype =
 
             {
                 this.chunck.prerender(node);
-                this.chunck.draw(wireframe);   
+                this.chunck.draw(wireframe);
             }
 
             //Blurring
-            if ((!node.elevationLoaded || !node.textureLoaded) && node.parent !== undefined)
-            { 
-              this.draw(wireframe, frustum, node.parent, ext, delta /64 , tile);
+            if ((!node.elevationLoaded || !node.textureLoaded) && node.parent !== undefined) {
+                this.draw(wireframe, frustum, node.parent, ext, delta / 64, tile);
             }
 
-            if (!node.textureLoaded ) {
-                
-                node.getTexture(ext, function ()
-                {
-                    if(node.texture.image ===null)
+            if (!node.textureLoaded) {
+
+                node.getTexture(ext, function() {
+                    if (node.texture.image === null)
                         return;
 
                     node.textureLoaded = true;
-                    node.texture.image.removeEventListener("load",node.loadtextureHandler, false);
+                    node.texture.image.removeEventListener("load", node.loadtextureHandler, false);
                     node.texture.image = null;
                 });
             }
@@ -242,24 +222,20 @@ XMap.Quadtree.prototype =
             if (!node.elevationLoaded) {
                 this._Wms.prepareRequest(node);
 
-                node.getElevationFromWms(this._Wms.url, function ()
-                {
-                    if(node.elevation.image === null)
+                node.getElevationFromWms(this._Wms.url, function() {
+                    if (node.elevation.image === null)
                         return;
 
-                    node.elevationLoaded = true; 
+                    node.elevationLoaded = true;
                     node.elevation.image.removeEventListener("load", node.loadElevation, false);
                     node.elevation.image = null;
                 });
             }
 
-            
-        }
-        else
-        {
-            for (var i = 0; i < 4; i++) 
-            {    
-                this.draw(wireframe, frustum, node.child[i], ext, delta/2.0, tile); 
+
+        } else {
+            for (var i = 0; i < 4; i++) {
+                this.draw(wireframe, frustum, node.child[i], ext, delta / 2.0, tile);
             }
         }
     }
--- 3D1/Client/Scripts/XMap.js
+++ 3D1/Client/Scripts/XMap.js
@@ -1,35 +1,32 @@
-﻿XMap.Proxy= {
-    proxyUrl: 'http://localhost:8000/image?server=' 
+﻿XMap.Proxy = {
+    proxyUrl: 'http://localhost:8000/image?server='
 }
 
-XMap.Url =
-{
+XMap.Url = {
     apiOrto: 'http://localhost:8000/Image/GetOrtoFoto?',
     apiElevation: 'http://localhost:8000/Image/GetOrtoFoto?',
     tileUrlBing: { pre: XMap.Proxy.proxyUrl + "http://t0.tiles.virtualearth.net/tiles/a", su: ".jpeg?g=854&mkt=en-US&token=Anz84uRE1RULeLwuJ0qKu5amcu5rugRXy1vKc27wUaKVyIv1SVZrUjqaOfXJJoI0", tile: false },
     tileUrlStatensKartverk: { pre: XMap.Proxy.proxyUrl + "http://www.webatlas.no/maptiles/tiles/webatlas-orto-newup/wa_grid/", su: ".jpeg", tile: true },
-    tileUrlthunderForest: { pre:XMap.Proxy.proxyUrl + "http://tile.thunderforest.com/cycle/", su: ".png", tile: true },
+    tileUrlthunderForest: { pre: XMap.Proxy.proxyUrl + "http://tile.thunderforest.com/cycle/", su: ".png", tile: true },
     tileUrlStammen: { pre: XMap.Proxy.proxyUrl + "http://tile.stamen.com/watercolor/", su: ".png", tile: true },
     mapQuest: { pre: XMap.Proxy.proxyUrl + "http://otile4.mqcdn.com/tiles/1.0.0/sat/", su: ".png", tile: true },
     google: { pre: XMap.Proxy.proxyUrl + "http://mt1.google.com/vt/lyrs=y", su: "", tile: true },
     elevationUrl: { pre: XMap.Proxy.proxyUrl + "http://webutvikling.gisline.no/Elevation/", su: ".png", tile: true, special: true },
     weather: { pre: XMap.Proxy.proxyUrl + "http://2.tile.openweathermap.org/map/precipitation/", su: ".png", tile: true },
-    geocoding : 'http://api.opencagedata.com/geocode/v1/json?pretty=1&key=2532638cbbd3c34fe376ccdfd6a9bc6c&query='
+    geocoding: 'http://api.opencagedata.com/geocode/v1/json?pretty=1&key=2532638cbbd3c34fe376ccdfd6a9bc6c&query='
 };
 
 XMap.Canvas = {
     _canvas: {},
 
-    create: function() 
-    {
+    create: function() {
         this._canvas = document.createElement("canvas");
         this._canvas.id = "canvasId";
 
-        var mouseWheel = function (e) 
-        {
+        var mouseWheel = function(e) {
             if (XMap.Engine._engine.lastUpdateCall)
                 cancelAnimationFrame(XMap.Engine._engine.lastUpdateCall);
-            
+
             var delta = e.wheelDelta ? e.wheelDelta : -e.detail;
 
             if (delta > 0) {
@@ -40,7 +37,7 @@ XMap.Canvas = {
                 XMap.Engine._engine.birdCamera._zoomOut();
             }
 
-            XMap.Engine._engine.lastUpdateCall = requestAnimationFrame(function () {
+            XMap.Engine._engine.lastUpdateCall = requestAnimationFrame(function() {
                 XMap.Engine._engine.renderScene();
 
             });
@@ -49,7 +46,7 @@ XMap.Canvas = {
 
         var mouseDown = function(e) {
             XMap.DOM.Events.mousemove = true;
-             document.getElementById("autocomplete").style.display = 'none';
+
             XMap.DOM.Events.lastMouseX = e.clientX;
             XMap.DOM.Events.lastMouseY = e.clientY;
         };
@@ -63,25 +60,23 @@ XMap.Canvas = {
     }
 };
 
-XMap.Search =
-{
-    geocoding : new XMap.Geocoding(XMap.Url.geocoding)
+XMap.Search = {
+    geocoding: new XMap.Geocoding(XMap.Url.geocoding)
 }
 
-XMap.Engine =
-{
+XMap.Engine = {
     _engine: {},
-    
-    create: function () {
-        
+
+    create: function() {
+
 
         var canvas = XMap.Canvas.create();
 
         var engineOptions = {
-            'canvas':canvas,
-            'tileUrl':XMap.Url.tileUrlBing,
-            'elevationUrl':XMap.Url.elevationUrl,
-             initialRootSize : 512
+            'canvas': canvas,
+            'tileUrl': XMap.Url.tileUrlBing,
+            'elevationUrl': XMap.Url.elevationUrl,
+            initialRootSize: 512
         };
 
         this._engine = new XMap.ThreeDEngine(engineOptions);
@@ -89,23 +84,21 @@ XMap.Engine =
         this._engine.tileUrl = XMap.Url.tileUrlStatensKartverk;
     },
 
-    render : function() {
+    render: function() {
         this._engine.renderScene();
 
     }
 };
 
-XMap.DOM = {}; 
+XMap.DOM = {};
 
-XMap.DOM.Events =
-{
+XMap.DOM.Events = {
     _toggleOn: true,
-    lastMouseX : 0,
-    lastMouseY : 0,
-    mousemove : false,
+    lastMouseX: 0,
+    lastMouseY: 0,
+    mousemove: false,
 
-    _displayPosition: function ()
-    {
+    _displayPosition: function() {
         document.getElementById('positionx').innerHTML = XMap.Engine._engine.camera.position[0].toFixed(2);
         document.getElementById('positiony').innerHTML = XMap.Engine._engine.camera.position[1].toFixed(2);
         document.getElementById('positionz').innerHTML = XMap.Engine._engine.camera.position[2].toFixed(2);
@@ -120,29 +113,26 @@ XMap.DOM.Events =
         else
             XMap.Engine._engine.wireFrame = device.TRIANGLES;
 
-         
+
         if (document.getElementById('bird-cam').checked)
             XMap.Engine._engine._birdCamOn = true;
         else
             XMap.Engine._engine._birdCamOn = false;
-            
+
     },
 
-    register: function()
-    {
-        document.getElementById("menu").addEventListener("click", function () { XMap.DOM.Events.menuonclick(); }, false);
-        document.getElementById("bing-tile").addEventListener("click", function () { XMap.Engine._engine.setTileUrl(XMap.Url.tileUrlBing); }, false);
-        document.getElementById("sk-tile").addEventListener("click", function () { XMap.Engine._engine.setTileUrl(XMap.Url.tileUrlStatensKartverk); }, false);
-        document.getElementById("thunderforest-tile").addEventListener("click", function () { XMap.Engine._engine.setTileUrl(XMap.Url.tileUrlthunderForest); }, false);
-        document.getElementById("tileStammen-tile").addEventListener("click", function () { XMap.Engine._engine.tileUrl = XMap.Url.tileUrlStammen; }, false);
-        document.getElementById("google").addEventListener("click", function () { XMap.Engine._engine.tileUrl = XMap.Url.google; }, false);
-        document.getElementById("Wms").addEventListener("click", function () { XMap.Engine._engine.Wms = true; }, false);
+    register: function() {
+        document.getElementById("menu").addEventListener("click", function() { XMap.DOM.Events.menuonclick(); }, false);
+        document.getElementById("bing-tile").addEventListener("click", function() { XMap.Engine._engine.setTileUrl(XMap.Url.tileUrlBing); }, false);
+        document.getElementById("sk-tile").addEventListener("click", function() { XMap.Engine._engine.setTileUrl(XMap.Url.tileUrlStatensKartverk); }, false);
+        document.getElementById("thunderforest-tile").addEventListener("click", function() { XMap.Engine._engine.setTileUrl(XMap.Url.tileUrlthunderForest); }, false);
+        document.getElementById("Wms").addEventListener("click", function() { XMap.Engine._engine.Wms = true; }, false);
 
-        document.getElementById("bbox").addEventListener("click", function () {
-        if (document.getElementById("bbox").checked)
-            XMap.Engine._engine.showBBox = true;
-        else
-            XMap.Engine._engine.showBBox = false;
+        document.getElementById("bbox").addEventListener("click", function() {
+            if (document.getElementById("bbox").checked)
+                XMap.Engine._engine.showBBox = true;
+            else
+                XMap.Engine._engine.showBBox = false;
         }, false);
 
         document.getElementById("spherify").addEventListener("click", function() {
@@ -159,20 +149,25 @@ XMap.DOM.Events =
             else
                 XMap.Engine._engine._birdCamOn = false;
         }, false);
-    
 
-        document.getElementById("mylocation").addEventListener("click", function () { XMap.Engine._engine._getUserPosition(); }, false);
-        document.getElementById("zoomin").addEventListener("click", function () { XMap.Engine._engine.camera._zoomIn(); XMap.Engine._engine.birdCamera._zoomIn();}, false);
-        document.getElementById("zoomout").addEventListener("click", function () { XMap.Engine._engine.camera._zoomOut(); XMap.Engine._engine.birdCamera._zoomOut();}, false);
-        document.getElementById("lookdown").addEventListener("click", function () { XMap.Engine._engine.camera.lookDown(); }, false);
-        document.getElementById("lookup").addEventListener("click", function () { XMap.Engine._engine.camera.lookUp(); }, false);
-        
-        document.addEventListener("mouseup", function(){
+
+        document.getElementById("mylocation").addEventListener("click", function() { XMap.Engine._engine._getUserPosition(); }, false);
+        document.getElementById("zoomin").addEventListener("click", function() {
+            XMap.Engine._engine.camera._zoomIn();
+            XMap.Engine._engine.birdCamera._zoomIn();
+        }, false);
+        document.getElementById("zoomout").addEventListener("click", function() {
+            XMap.Engine._engine.camera._zoomOut();
+            XMap.Engine._engine.birdCamera._zoomOut();
+        }, false);
+        document.getElementById("lookdown").addEventListener("click", function() { XMap.Engine._engine.camera.lookDown(); }, false);
+        document.getElementById("lookup").addEventListener("click", function() { XMap.Engine._engine.camera.lookUp(); }, false);
+
+        document.addEventListener("mouseup", function() {
             XMap.DOM.Events.mousemove = false;
         });
 
-        document.addEventListener("mousemove", function (e)
-        {
+        document.addEventListener("mousemove", function(e) {
             var localPositionX = 2.0 * e.clientX / XMap.Canvas._canvas.width - 1.0;
             var localPositionY = -2.0 * e.clientY / XMap.Canvas._canvas.height + 1.0;
             var localPositionZ = -1.0;
@@ -207,48 +202,17 @@ XMap.DOM.Events =
 
             if (XMap.Engine._engine.lastUpdateCall)
                 cancelAnimationFrame(XMap.Engine._engine.lastUpdateCall);
-            
+
             XMap.Engine._engine.lastUpdateCall = requestAnimationFrame(function() {
                 XMap.Engine._engine.renderScene();
             });
         });
-        
 
-        document.getElementById("coordinate").addEventListener("keyup", function (e) {
-            
-            document.getElementById("floating-div").style.transform = "translate(-440px)";
-            this._toggleOn = true;
 
-            document.getElementById("autocomplete").style.display = 'block';
 
-            var searchString = document.getElementById("coordinate").value;
 
-            if (searchString === "")
-                return;
-
-           XMap.Search.geocoding.constructSearchUrl(searchString, '', '');
-
-            var jObj = {};
- 
-            var autocomplete = document.getElementById('autocomplete');
-
-            while (autocomplete.firstChild)
-            {
-                autocomplete.removeChild(autocomplete.firstChild);
-            }
-
-            XMap.Search.geocoding.sendRequest(function (resp) {
-
-                if (resp !== '') {
-                    jObj = JSON.parse(resp);
-                    XMap.DOM.Events._generateAutocomplete(jObj);
-                }
-            });
-
-        });
-
-        window.addEventListener("resize", function(){
-            var displayWidth  = window.innerWidth;
+        window.addEventListener("resize", function() {
+            var displayWidth = window.innerWidth;
             var displayHeight = window.innerHeight;
 
 
@@ -256,70 +220,14 @@ XMap.DOM.Events =
                 XMap.Canvas._canvas.height !== displayHeight) {
 
 
-                XMap.Canvas._canvas.width  = displayWidth;
+                XMap.Canvas._canvas.width = displayWidth;
                 XMap.Canvas._canvas.height = displayHeight;
-                
+
 
                 window.device.viewport(0, 0, XMap.Canvas._canvas.width, XMap.Canvas._canvas.height);
             }
 
         });
-    },
-
-
-    _generateAutocomplete: function (obj) {
-        if (obj === null)
-            return;
-        
-        for (var i = 0; i < obj.results.length; i++)
-        {
-            var listElement = document.createElement('li');
-
-            var img = document.createElement('img');
-            img.src = '../Images/pin_marker-256.png';
-
-            var country = document.createElement('span');
-            country.textContent = obj.results[i].components.country;
-
-            listElement.textContent = obj.results[i].formatted;
-            listElement.id = 'list-element-' + i;
-            listElement.appendChild(img);
-            listElement.appendChild(country);
-
-            listElement.lat = obj.results[i].geometry.lat;
-            listElement.lng = obj.results[i].geometry.lng;
-            listElement.cityName = obj.results[i].components.country;
-
-            
-            listElement.addEventListener("click", function ()
-            {
-                if (XMap.Engine._engine.lastUpdateCall)
-                    cancelAnimationFrame(XMap.Engine._engine.lastUpdateCall);
-
-                var li = document.getElementById(this.id);
-                var mercator = new XMap.Mercator(XMap.Engine._engine.initialRootSize);
-
-                var x = mercator.getX(li.lng);
-                var z = mercator.getZ(li.lat);
-
-                XMap.Engine._engine.camera.position = vec3.create([x, 0.05, z]);
-                XMap.Engine._engine.camera.lookAt = vec3.create([x, -1.05, z]);
-                XMap.Engine._engine.camera.pitch =   Math.PI;
-                XMap.Engine._engine.camera.update();
-
-                document.getElementById("coordinate").value = li.cityName;
-                document.getElementById('autocomplete').innerHTML = '';
-
-                XMap.Engine._engine.lastUpdateCall = requestAnimationFrame(function() {
-                XMap.Engine._engine.renderScene();
-                
-            });
-
-            });
-
-            document.getElementById('autocomplete').appendChild(listElement);
-            
-        }
     },
 
     menuonclick: function() {
@@ -332,9 +240,12 @@ XMap.DOM.Events =
         }
     }
 
-    
+
 };
 
 
-window.addEventListener("load", function() { XMap.Engine.create(); XMap.Engine.render(); XMap.DOM.Events.register()}, false);
-   
+window.addEventListener("load", function() {
+    XMap.Engine.create();
+    XMap.Engine.render();
+    XMap.DOM.Events.register()
+}, false);
